98d5bf29d04d571c4a54f1a524253f8ca7a133e7
lakshay-bhatnagar <lakshaybhatnagar22@gmail.com>
2024-10-23 22:32:22 +0530

    added binding dependency

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -25,18 +25,30 @@ android {
             )
         }
     }
+
     compileOptions {
         sourceCompatibility = JavaVersion.VERSION_1_8
         targetCompatibility = JavaVersion.VERSION_1_8
     }
+
+    // Enable Data Binding
+    buildFeatures {
+        dataBinding = true
+    }
+
+    // Enable View Binding
+    viewBinding {
+        enable = true
+    }
 }
 
 dependencies {
-
     implementation(libs.appcompat)
     implementation(libs.material)
     implementation(libs.activity)
     implementation(libs.constraintlayout)
+    implementation(libs.navigation.fragment)
+    implementation(libs.navigation.ui)
     testImplementation(libs.junit)
     androidTestImplementation(libs.ext.junit)
     androidTestImplementation(libs.espresso.core)
